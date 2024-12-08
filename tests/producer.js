f4dae0fd5ab527e2515211c24721465d54afce53
Christos <cioa15@yahoo.gr>
2024-12-08 23:20:42 +0200

    SSE library added and loop test

--- tests/producer.js
+++ tests/producer.js
@@ -1,19 +1,28 @@
 const { Kafka } = require("kafkajs");
+//const EventSource = require("eventsource"); for Server-Sent Events (SSE)
 
 const kafka = new Kafka({
-  clientId: "greeting-service",
+  clientId: "test-service",
   brokers: ["server"],
 });
 
 const producer = kafka.producer();
+let i = 0;
 
 const run = async () => {
   await producer.connect();
-  await producer.send({
-    topic: "mytopic",
-    messages: [{ value: "Hello from greeting-service!" }],
-  });
-  await producer.disconnect();
+
+  try {
+    while (true) {
+      await producer.send({
+        topic: "mytopic",
+        messages: [{ value: "Hello from test-service!" + i++ }],
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    await producer.disconnect();
+  }
 };
 
 run().catch(console.error);
